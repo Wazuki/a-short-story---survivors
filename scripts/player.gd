a36d1173d076a5e067d25d991ad8199f892bfe4b
Kyle Costa <kpcosta3@gmail.com>
2025-01-27 01:07:27 -0500

    New sprites introduced plus player death animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,34 +20,44 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	#GetVector() turns movement into 2D direction
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * speed
+
+ 	# If the player is alive, move them based on input. This is also where we will fire weapons, gain XP, etc.
+	if health > 0:
+		var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		velocity = direction * speed
+		
+		move_and_slide() # Automatically moves character based on velocity. Applies delta automatically
+		
+		# If the player is moving, play the walk animation. Otherwise, play the idle animation. Flip the sprite based on direction.
+		if velocity.length() > 0:
+			%Spritesheet.animation = "walk"
+			if velocity.x < 0:
+				%Spritesheet.flip_h = true
+			elif velocity.x > 0:
+				%Spritesheet.flip_h = false
+			%Spritesheet.play()
+		else: %Spritesheet.animation = "idle"
 	
-	move_and_slide() # Automatically moves character based on velocity. Applies delta automatically
-	
-	if velocity.length() > 0:
-		%Spritesheet.animation = "player_walk"
-		%Spritesheet.play()
-	else: %Spritesheet.animation = "player_idle"
-	
-	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
-	
-	if not overlapping_mobs.is_empty():
-		health -= enemy_damage_rate * overlapping_mobs.size() * delta # Deal damage for each mob touching the player times delta so they don't explode
-		%HealthBar.value = health
-	
-	update_player_info_text()
-	
-	if health <= 0:
+		var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+		
+		if not overlapping_mobs.is_empty():
+			health -= enemy_damage_rate * overlapping_mobs.size() * delta # Deal damage for each mob touching the player times delta so they don't explode
+			%HealthBar.value = health
+			# Firing weapons moved to each weapon function to make them independent of the player.
+			
+		var xp_to_absorb = %PickupRadius.get_overlapping_areas()
+		# print_debug("There are " + str(xp_to_absorb.size()) + " xp in range!")
+		for xp in xp_to_absorb:
+			xp.absorbing = true # Call the Absorb function on the XP so they fly towards the player
+			if not %XPPickupSound.playing: %XPPickupSound.play() # Play the XP sound but only if it's not currently playing to rpevent spam
+
+		update_player_info_text()
+	# If the player is dead, play the death animation and emit the signal to the game controller.
+	else:
 		#  Kill the player at zero health.
-		emit_signal("_health_depleted")
-	
-	# Firing weapons moved to each weapon function to make them independent of the player.
-	var xp_to_absorb = %PickupRadius.get_overlapping_areas()
-	# print_debug("There are " + str(xp_to_absorb.size()) + " xp in range!")
-	for xp in xp_to_absorb:
-		xp.absorbing = true # Call the Absorb function on the XP so they fly towards the player
-		if not %XPPickupSound.playing: %XPPickupSound.play() # Play the XP sound but only if it's not currently playing to rpevent spam
+		# emit_signal("_health_depleted")
+		%Spritesheet.animation = "death"
+		%Spritesheet.play()
 
 func get_target() -> Vector2:
 	# Return the first mob that overlaps the weapon range collider - TODO: adjustable range?
@@ -89,6 +99,9 @@ func reset() -> void:
 	# Don't forget to reset the UI!
 	%HealthBar.value = health
 	update_player_info_text()
+
+	%Spritesheet.animation = "idle"
+	%Spritesheet.play()
 	
 func update_player_info_text() -> void:
 	var text = "Level: " + str(level)
@@ -96,3 +109,12 @@ func update_player_info_text() -> void:
 	text += "\nTNL: " + str(xp_to_next_level)
 	text += "\nHP: " + str(roundi(health))
 	player_info_text.text = text
+
+
+func _on_spritesheet_animation_finished() -> void:
+	# Tell the UI we're dead
+	print_debug("Our animation is " + %Spritesheet.animation)
+	if %Spritesheet.animation == "death":
+		print("Player died!")
+		emit_signal("_health_depleted")
+		%Spritesheet.stop()
